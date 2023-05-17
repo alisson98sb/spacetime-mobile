13d4e3318a01073735bcfb3ad5e9548e8d6d8d3c
alisson98sb <alisson98sb@gmail.com>
2023-05-17 15:41:09 -0300

    Criei o setup do projeto, instalei e configurei o tailwind, o prettier, ts e eslint

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-950">
+      <Text className="">Hello world</Text>
+    </View>
+  )
+}
