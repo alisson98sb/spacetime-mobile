fec302622a85227e04cfc0062a280528dcfeae02
alisson98sb <alisson98sb@gmail.com>
2023-05-22 15:11:33 -0300

    finished homepage

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { ImageBackground } from 'react-native'
+import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import {
   useFonts,
@@ -11,6 +11,8 @@ import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 import blurbg from './src/assets/bg-blur.png'
 
 import Stripes from './src/assets/stripes.svg'
+import NlwLogo from './src/assets/nlw-spacetime-logo.svg'
+
 import { styled } from 'nativewind'
 
 export default function App() {
@@ -27,10 +29,35 @@ export default function App() {
   return (
     <ImageBackground
       source={blurbg}
-      className="relative flex-1 items-center bg-gray-900"
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NlwLogo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-50">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
       <StatusBar translucent style="light" />
     </ImageBackground>
   )
