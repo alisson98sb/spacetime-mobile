cede917849b610ced6d0d561081ef608a104e74e
alisson98sb <alisson98sb@gmail.com>
2023-05-22 14:30:07 -0300

    Configuração das fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,27 @@
 import { Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="">Hello world</Text>
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="font-alt text-5xl text-gray-50">Hello world</Text>
+      <StatusBar translucent style="light" />
     </View>
   )
 }
